74f111f6887ead3742e2581eda38a2af4fbc8073
Carlos Gortaris <cgortaris@gmail.com>
2014-06-09 23:38:31 +0000

    Updated API

--- index.php
+++ index.php
@@ -1,21 +1,43 @@
 <?php
+// Database configuration
+require_once('../../dbconfig.php');
+
 class SMB {
-    private $db;
     private $json_sources = './json/';
+    private $conn;
+    private $stmts;
  
-    // Constructor
     function __construct() {
-//        $this->db = new mysqli('localhost', 'username', 'password', 'db');
-//        $this->db->autocommit(FALSE);
+        $this->connect();
     }
  
-    // Destructor
     function __destruct() {
-//        $this->db->close();
+        $this->disconnect();
+    }
+
+    private function connect() {
+        $this->conn = new PDO(sprintf('pgsql:dbname=%s;host=%s', DBNAME, DBHOST), DBUSER, DBPASS, array(PDO::ATTR_PERSISTENT => true));
+        $this->stmts = array(
+            'players'   => $this->getConn()->prepare('select p.id as pid, p.name as player,t.name as team,t.id as tid,s.type as type, s.sentiment from stats_players s join players p on (s.player_id=p.id) join team t on (s.team_id=t.id) where t.code = ? order by t.name,s.type,s.sentiment desc'),
+            'teams'     => $this->getConn()->prepare('select t.name as team, s.sentiment as sentiment from stats_teams s join team t on (s.team_id=t.id) where t.code = ?')
+//            'tt'        => $this->getConn()->prepare('')
+        );
+    }
+
+    private function disconnect() {
+        $this->conn = null;
+    }
+
+    public function getConn() {
+        return $this->conn;
+    }
+
+    public function getStmts() {
+        return $this->stmts;
     }
 
     // Ver qué información se está pidiendo y llamar a la función correspondiente
-    function proceed() {
+    public function proceed() {
         if (isset($_GET['mid']) && isset($_GET['type'])) {
             switch($_GET['type']) {
                 case 'players':
@@ -31,22 +53,104 @@ class SMB {
         }
     }
 
-    function players() {
-        $this->sendJSON('players.json');
+    private function players($mid) {
+        if($this->validateMatchID($mid)) {
+            $team_codes = explode('-', $mid, 2);
+            
+            // get players' sentiment
+            $players_sentiment_1 = $this->getPlayersSentiment($team_codes[0]);
+            $players_sentiment_2 = $this->getPlayersSentiment($team_codes[1]);
+            $data = array(
+                'match-id' => $mid,
+                'players' => array(
+                    $players_sentiment_1,
+                    $players_sentiment_2
+                )
+            );
+            $this->sendJSON($data);
+        }
     }
 
-    function teams() {
-        $this->sendJSON('teams.json');
+    private function teams($mid) {
+        if($this->validateMatchID($mid)) {
+            $team_codes = explode('-', $mid, 2);
+            $teams = array(
+                $this->getTeamSentiment($team_codes[0]),
+                $this->getTeamSentiment($team_codes[1])
+            );
+            $data = array(
+                'match-id' => $mid,
+                'teams' => $teams
+            );
+            // get teams' sentiment
+            $this->sendJSON($data);
+        }
     }
 
-    function trending_topics() {
-        $this->sendJSON('tt.json');
+    private function trending_topics() {
     }
 
-    function sendJSON($filename) {
-        $json_filename = $this->json_sources.$filename;
-        $json = json_decode(file_get_contents($json_filename), true);
-        sendResponse(200, json_encode($json));
+    private function getTeamSentiment($tc) {
+        $sentiment = 50;
+        $stmt = $this->getStmts()['teams'];
+        if($stmt->execute(array($tc))) {
+            $result = $stmt->fetchAll();
+            $sentiment = $result[0]['sentiment'];
+        }
+        
+        return array(
+            'id' => $tc,
+            'sentiment' => $sentiment
+        );
+    }
+
+    private function getPlayersSentiment($tc) {
+        $top = array();
+        $worst = array();
+        $stmt = $this->getStmts()['players'];
+        if($stmt->execute(array($tc))) {
+            $result = $stmt->fetchAll();
+            foreach($result as $p) {
+                $player_name = $p['player'];
+                $type = $p['type'];
+                $sentiment = $p['sentiment'];
+                $player = array('player_name' => $player_name, 'sentiment' => $sentiment);
+                switch($type) {
+                    case 'top':
+                        $top[] = $player;
+                        break;
+                    case 'worst':
+                        $worst[] = $player;
+                        break;
+                }
+            }
+        }
+        return array('team' => $tc, 'top3+' => $top, 'top3-' => $worst);
+    }
+    
+    private function sendJSON($data) {
+        sendResponse(200, json_encode($data), 'application/json');
+    }
+    
+    private function error($msg) {
+        sendResponse(400, json_encode(array('error_message' => $msg)));
+    }
+    
+    /**
+      * Match ID should comply with:
+      * - 7 characters
+      * - a dash in the middle
+      * - only uppercase A-Z characters contained on the three at left and right sides
+      */
+    private function validateMatchID($m) {
+        $pattern = '/[A-Z]{3}-[A-Z]{3}/';
+        if(preg_match($pattern, $m)) {
+            return true;
+        }
+        else {
+            $this->error("Wrong match code '$m'. Expected format is 'ABC-EFG': two sets of three upper case letters separated by a dash. Each set must match a FIFA team code.");
+            return false;
+        }
     }
 }
 
